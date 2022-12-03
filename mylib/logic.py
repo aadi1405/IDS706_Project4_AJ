4984dc8b41fc2ed341870b9f363481bf9a9e70b0
AADILA JASMIN <67281453+aadi1405@users.noreply.github.com>
2022-12-03 03:55:36 +0000

    adding a new cli func

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,13 @@ def wiki(name="2022_FIFA_World_Cup", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    # this is a wikipedia search
+    results = wikipedia.search(name)
+    return results
+
+def page_wiki(name):
+    # this is a wikipedia page
+    page = wikipedia.page(name)
+    return page
